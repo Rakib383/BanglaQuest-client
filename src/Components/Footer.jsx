8ded9920bd25343dba06a24469ac85c3af000fd0
Rakib383 <sarkar12ab@gmail.com>
2025-01-16 18:20:20 +0600

    added footer section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,36 @@
+import logo from "../assets/Images/BanglaQuest.png"
 export const Footer = () => {
     return (
-        <div></div>
+        <div className="pb-5 bg-gradient-to-t from-SecondaryColor to-white text-center ">
+            <footer className="gap-5 flex flex-col sm:flex-row  sm:justify-center sm:gap-12 footer-center 
+              p-10 mt-20 max-w-7xl mx-auto  ">
+                <aside>
+                    <img src={logo} className="w-12" alt="" />
+                    <div>
+                        <p className=" text-3xl font-fresh text-[#FFB116] "><span className="text-[#0F1325] text-2xl">Bangla</span>Quest</p>
+                        <span className="font-oswald text-base"> Unveiling Bangladesh&apos;s Wonders</span>
+                    </div>
+
+                </aside>
+                <nav className="flex justify-around gap-8  sm:w-3/5">
+                    <div className="flex flex-col">
+                        <h6 className="footer-title">Services</h6>
+                        <a className="link link-hover">Tour Packages</a>
+                        <a className="link link-hover">Tour Guide</a>
+                        <a className="link link-hover">Travel Planning</a>
+                        <a className="link link-hover">Customer Support</a>
+                    </div>
+                    <div className="flex flex-col">
+                        <h6 className="footer-title">Company</h6>
+                        <a className="link link-hover">About us</a>
+                        <a className="link link-hover">Contact</a>
+                        <a className="link link-hover">Our Team</a>
+                        <a className="link link-hover">FAQs</a>
+                    </div>
+                </nav>
+
+            </footer>
+            <p className="text-white">Copyright © {new Date().getFullYear()} - All rights reserved</p>
+        </div>
     )
 }
--- src/Components/NewsLetter.jsx
+++ src/Components/NewsLetter.jsx
@@ -7,7 +7,7 @@ export const NewsLetter = () => {
             backgroundPosition: 'center',
            
         }}>
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[300px] md:w-96  rounded-lg bg-white/40 backdrop-blur-md px-6 py-3.5 md:py-7 text-center">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[300px] sm:w-96  rounded-lg bg-white/40 backdrop-blur-md px-6 py-3.5 md:py-7 text-center">
                 <h3 className="text-PrimaryColor text-lg md:text-xl">Stay Updated with Our Newsletters</h3>
                 <p className="text-sm md:text-base my-1">Get the latest updates, exclusive offers, and travel inspiration directly to your inbox!</p>
                 <div className="mt-3 space-y-3 md:space-y-0 md:mt-5 md:flex gap-3 items-center ">
